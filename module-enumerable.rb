d08bae2b76e3857d9733447f5a2fac5bde79f8b1
udeaghad <dozie.udeagha@gmail.com>
2022-08-25 16:50:42 +0100

    Create enumerable module

---
+++ module-enumerable.rb
@@ -0,0 +1,25 @@
+module Enumerable
+  def all?
+    result = true
+
+    each { |item| result = false unless yield item }
+
+    result
+  end
+
+  def any?
+    result = false
+
+    each { |item| result = true if yield item }
+
+    result
+  end
+
+  def filter
+    result = []
+
+    each { |item| result.push(item) if yield item }
+
+    result
+  end
+end
